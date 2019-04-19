9bb201d294a2dd018c273e880fa20b5d66c281bc
yagnashreeb <45913165+yagnahreeb@users.noreply.github.com>
2019-04-19 12:44:07 +0530

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MainareaComponent } from './mainarea/mainarea.component';
 import { RouterModule } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent, MainareaComponent],
   imports: [
-    CommonModule,
+    CommonModule,MatMenuModule,MatButtonModule,
     RouterModule.forChild([])
   ],
   exports:[ HeaderComponent, MainareaComponent]
